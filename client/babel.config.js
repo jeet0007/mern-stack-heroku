b7accb94e9138f7df9a91af62130d0809f05b7e2
Jeet Singh <taranjeet0007@gmail.com>
2021-02-11 20:32:01 +0700

    Testing jenkins- Init Testing branch

---
+++ client/babel.config.js
@@ -0,0 +1,20 @@
+// babel.config.js
+module.exports = {
+    presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+};
+
+module.exports = api => {
+    const isTest = api.env('test');
+    // You can use isTest to determine what presets and plugins to use.
+
+    return {
+        // ...
+    };
+};
+
+module.exports = {
+    presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        +   '@babel/preset-typescript',
+    ],
+};
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
